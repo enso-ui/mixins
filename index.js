8d510f09c6b9ec24665a90b753dc49a0d6d6d3fe
mishu <mishu@xtelecom.ro>
2019-07-07 11:00:16 +0300

    adds files mixin

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import canAccess from './src/canAccess';
 import errorHandler from './src/errorHandler';
 import i18n from './src/i18n';
+import files from './src/files';
 
 export {
-    canAccess, errorHandler, i18n,
+    canAccess, errorHandler, i18n, files,
 };
