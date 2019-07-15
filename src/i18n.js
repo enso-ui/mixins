4c540919441b4cbd19b2d14ffde154f03c33147f
mishu <mishu@xtelecom.ro>
2019-07-15 08:05:08 +0300

    extracts i18n in core modules

--- src/i18n.js
+++ src/i18n.js
@@ -1,41 +1,9 @@
+import translator from '@core-modules/plugins/i18n';
+
 export default {
     methods: {
         i18n(key, params = null) {
-            if (key === null || key === '' || typeof key === 'undefined') {
-                return null;
-            }
-
-            if (!this.$store.getters['localisation/ready']) {
-                return key;
-            }
-
-            let translation = this.$store.getters['localisation/i18n'](key);
-
-            if (typeof translation === 'undefined' || translation === null) {
-                translation = key;
-
-                if (this.$store.state.localisation.keyCollector) {
-                    this.$store.commit('localisation/addMissingKey', key);
-                }
-            }
-
-            return !!params && typeof params === 'object'
-                ? translation.replace(/:(\w*)/g, (e, key) => {
-                    if (typeof params[key.toLowerCase()] === 'undefined') {
-                        return key;
-                    }
-
-                    const param = params[key.toLowerCase()];
-
-                    if (key === key.toUpperCase()) {
-                        return param.toUpperCase();
-                    }
-
-                    return key[0] === key[0].toUpperCase()
-                        ? param.charAt(0).toUpperCase() + param.slice(1)
-                        : param;
-                })
-                : translation || key;
+            return translator(key, params);
         },
     },
 };
