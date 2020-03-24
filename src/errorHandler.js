c3beaf405219040f2309cfc4ca86a7396f2c7a42
mishu <mishu@xtelecom.ro>
2020-03-24 12:26:35 +0200

    makes feedback form available only in production

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -1,22 +1,26 @@
 import { showReportDialog } from '@sentry/browser';
 
+const toastError = vm => vm.$toastr.error(vm.i18n('Something went wrong...'));
+
+const dialog = (vm, eventId) => ({
+    eventId,
+    title: vm.i18n('It looks like we’re having issues.'),
+    subtitle: vm.i18n('Our team has been notified.'),
+    subtitle2: vm.i18n('If you’d like to help, tell us what happened below.'),
+    labelName: vm.i18n('Name'),
+    labelEmail: vm.i18n('Email'),
+    labelComments: vm.i18n('What happened?'),
+    labelClose: vm.i18n('Close'),
+    labelSubmit: vm.i18n('Submit'),
+    errorGeneric: vm.i18n('An unknown error occurred while submitting your report. Please try again.'),
+    errorFormEntry: vm.i18n('Some fields were invalid. Please correct the errors and try again.'),
+});
+
 const getUserFeedback = vm => axios.get('api/sentry').then(({ data }) => {
     if (data.eventId) {
-        showReportDialog({
-            eventId: data.eventId,
-            title: vm.i18n('It looks like we’re having issues.'),
-            subtitle: vm.i18n('Our team has been notified.'),
-            subtitle2: vm.i18n('If you’d like to help, tell us what happened below.'),
-            labelName: vm.i18n('Name'),
-            labelEmail: vm.i18n('Email'),
-            labelComments: vm.i18n('What happened?'),
-            labelClose: vm.i18n('Close'),
-            labelSubmit: vm.i18n('Submit'),
-            errorGeneric: vm.i18n('An unknown error occurred while submitting your report. Please try again.'),
-            errorFormEntry: vm.i18n('Some fields were invalid. Please correct the errors and try again.'),
-        });
+        showReportDialog(dialog(vm, data.eventId));
     } else {
-        vm.$toastr.error(vm.i18n('Something went wrong...'));
+        toastError(vm);
     }
 });
 
@@ -50,7 +54,7 @@ export default {
             }
 
             if (shouldDisplayToastr(status)) {
-                this.$toastr.error(data.message);
+                this.$toastr.warning(this.i18n(data.message));
                 return;
             }
 
@@ -61,9 +65,14 @@ export default {
 
             if (maintenanceMode(status)) { // TODO add front-end maintenance mode page
                 window.location.reload();
+                return;
             }
 
-            getUserFeedback(this);
+            if (this.$store.state.meta.env === 'production') {
+                getUserFeedback(this);
+            } else {
+                toastError(this);
+            }
         },
     },
 };
