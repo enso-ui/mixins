15e98fe9058e16ec41ed2c96015b9f0e6ea82753
mishu <mishu@xtelecom.ro>
2020-03-24 11:45:01 +0200

    adds translation in user feedback

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -1,3 +1,25 @@
+import { showReportDialog } from '@sentry/browser';
+
+const getUserFeedback = vm => axios.get('api/sentry').then(({ data }) => {
+    if (data.eventId) {
+        showReportDialog({
+            eventId: data.eventId,
+            title: vm.i18n('It looks like we’re having issues.'),
+            subtitle: vm.i18n('Our team has been notified.'),
+            subtitle2: vm.i18n('If you’d like to help, tell us what happened below.'),
+            labelName: vm.i18n('Name'),
+            labelEmail: vm.i18n('Email'),
+            labelComments: vm.i18n('What happened?'),
+            labelClose: vm.i18n('Close'),
+            labelSubmit: vm.i18n('Submit'),
+            errorGeneric: vm.i18n('An unknown error occurred while submitting your report. Please try again.'),
+            errorFormEntry: vm.i18n('Some fields were invalid. Please correct the errors and try again.'),
+        });
+    } else {
+        vm.$toastr.error(vm.i18n('Something went wrong...'));
+    }
+});
+
 const sessionExpired = (vm, status) => [401, 419].includes(status) && vm.$store.state.auth.isAuth;
 
 const redirectToLogin = (vm) => {
@@ -37,13 +59,11 @@ export default {
                 return;
             }
 
-            if (maintenanceMode(status)) {
+            if (maintenanceMode(status)) { // TODO add front-end maintenance mode page
                 window.location.reload();
             }
 
-            this.$toastr.error(this.i18n('Something went wrong...'));
-
-            throw error;
+            getUserFeedback(this);
         },
     },
 };
