ab4b9e94bb614170d5601294063df6cefcc3b203
mishu <mishu@xtelecom.ro>
2020-06-11 12:54:06 +0300

    removes unneeded dep

--- src/filters/numberFormat.js
+++ src/filters/numberFormat.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
-import store from '@root/store';
 
 Vue.filter('numberFormat', (value, decimals = 3) => new Intl.NumberFormat(
-    store.state.preferences.global.lang, {
+    this.$store.state.preferences.global.lang, {
         style: 'decimal',
         minimumFractionDigits: decimals,
         maximumFractionDigits: decimals,
