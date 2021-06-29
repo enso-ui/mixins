282b55e8a022e0c176b440bd256098e3d0617766
mishu <mishu@xtelecom.ro>
2021-06-29 11:07:10 +0300

    adds router error handler

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import canAccess from './src/canAccess';
 import errorHandler from './src/errorHandler';
 import files from './src/files';
+import routerErrorHandler from './src/routerErrorHandler';
 import shortNumber from './src/filters/shortNumber';
 
 export {
-    canAccess, errorHandler, files, shortNumber,
+    canAccess, errorHandler, files, routerErrorHandler, shortNumber,
 };
