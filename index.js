a69a9900376250a0b376c7ff93338fc9d521ddb4
Mihai <mihai.ocneanu@gmail.com>
2021-12-23 15:10:42 +0200

    wip

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-import canAccess from './src/canAccess';
-import errorHandler from './src/errorHandler';
-import files from './src/files';
-import numberFormat from './src/filters/numberFormat';
-import routerErrorHandler from './src/routerErrorHandler';
-import shortNumber from './src/filters/shortNumber';
+import EnsoFile from './src/modules/ensoFile';
+import numberFormat from './src/modules/numberFormat';
+import shortNumber from './src/modules/shortNumber';
 
 export {
-    canAccess, errorHandler, files, numberFormat,
-    routerErrorHandler, shortNumber,
+    EnsoFile, numberFormat, shortNumber,
 };
--- src/modules/numberFormat.js
+++ src/modules/numberFormat.js
@@ -1,4 +1,4 @@
-import store from '../../../ui/src/core/services/store';
+import store from '@enso-ui/ui/src/core/services/store';
 
 export default (value, decimals = 3) => new Intl.NumberFormat(
     store.state.preferences.global.lang, {
